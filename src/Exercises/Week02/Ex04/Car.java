0c899d8f7671a497933b0ed72bca6f78c54c3987
tabourshaid <tabourshaid@users.noreply.github.com>
2018-12-11 11:51:37 +0100

    Java_intake04

---
+++ src/Exercises/Week02/Ex04/Car.java
@@ -0,0 +1,13 @@
+package Exercises.Week02.Ex04;
+
+public class Car {
+    private String brand;
+
+    public Car(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+}
---
+++ src/Exercises/Week02/Ex04/Seller.java
@@ -0,0 +1,8 @@
+package Exercises.Week02.Ex04;
+
+public class Seller {
+    public void describe (Car car){
+        System.out.println("The "+ car.getBrand()+ " is particularly good.");
+
+    }
+}
