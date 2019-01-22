733302b362f6293a229c31eda4fdea0400054ed5
tammamabourshaid <tammam.abourshaid@gmail.com>
2019-01-22 16:57:14 +0100

    Refliction

--- src/Refliction/Refliction_190118/ReflictionApplication_190118.java
+++ src/Refliction/Refliction_190118/ReflictionApplication_190118.java
@@ -1,24 +1,25 @@
-package Refliction.Refliction_190118;
-
-import java.util.Comparator;
-import java.util.List;
-
-public class ReflictionApplication_190118 {
-    public static void main(String[] args) {
-
-        List<Student> studentList = new StudentReader().getData();
-        numberStudentPerormnce(studentList);
-    }
-
-    private static void numberStudentPerormnce(List<Student> studentList) {
-        System.out.println("Total number of student performance entries: ");
-        List<String> collect = studentList.stream()
-
-                .sorted(Comparator.comparing(Student::getGender).reversed())
-                .filter(e -> e.getGender().contains("female"))
-                .map(e -> e.getGender());
-        long total = collect.stream().distinct().count();
-        System.out.println(total);
-    }
-}
-
+//package Refliction.Refliction_190118;
+//
+//import java.util.Comparator;
+//import java.util.List;
+//
+//public class ReflictionApplication_190118 {
+//    public static void main(String[] args) {
+//
+//        List<Student> studentList = new StudentReader().getData();
+//        numberStudentPerormnce(studentList);
+//    }
+//
+//    private static void numberStudentPerormnce(List<Student> studentList) {
+//        System.out.println("Total number of student performance entries: ");
+//        List<String> collect = studentList.stream()
+//
+//                .sorted(Comparator.comparing(Student::getGender).reversed())
+//                .filter(e -> e.getGender().contains("female"))
+//                .map(e -> e.getGender());
+//        long total = collect.stream().distinct().count();
+//
+//        System.out.println(total);
+//    }
+//}
+//
