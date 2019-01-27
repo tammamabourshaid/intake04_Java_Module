e63f2570f81ffeca8ce0967d296a284f74df3914
tammamabourshaid <tammam.abourshaid@gmail.com>
2019-01-27 21:39:38 +0100

    Refliction

--- src/Refliction/Refliction_190125/AliceApp.java
+++ src/Refliction/Refliction_190125/AliceApp.java
@@ -13,10 +13,10 @@ public class AliceApp {
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        List<String> alice = fileReader.asList("Refliction/Refliction_190125/alice.txt");
+        Stream<String> alice = fileReader.asStream("Refliction/Refliction_190125/alice.txt");
 
 
-        List<String> collect = alice.stream()
+        List<String> collect = alice
                 .map(e -> e.replaceAll(",", ""))
                 .map(e -> e.replaceAll(";", ""))
                 .map(e -> e.replaceAll("’s", ""))
