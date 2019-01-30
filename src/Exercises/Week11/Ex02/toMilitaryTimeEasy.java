7e96c26a8365bfa70d59aca7e89e6ea91427dd7e
tammamabourshaid <tammam.abourshaid@gmail.com>
2019-01-30 15:31:43 +0100

    Java_intake04

--- src/Exercises/Week11/Ex02/toMilitaryTimeEasy.java
+++ src/Exercises/Week11/Ex02/toMilitaryTimeEasy.java
@@ -4,13 +4,13 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class toMilitaryTimeEasy {
-    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-
     public static void main(String[] args) {
         toMilitary("07:05:45PM");
     }
 
     private static void toMilitary(String time) {
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         LocalTime localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("hh:mm:ssa"));
         System.out.println(localTime.format(formatter));
     }
