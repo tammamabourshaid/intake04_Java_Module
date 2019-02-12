0e2d0426092dc124a91ee25045c9f7ac0d4b90e0
tammamabourshaid <tammam.abourshaid@gmail.com>
2019-02-12 10:51:51 +0100

    Java_intake04

--- src/Exercises/Week05/Ex02/Triangle.java
+++ src/Exercises/Week05/Ex02/Triangle.java
@@ -3,6 +3,7 @@ package Exercises.Week05.Ex02;
 import Exercises.Week05.Ex01.Shape;
 
 public class Triangle implements Shape {
+
     @Override
     public String getName() {
         return "triangle";
